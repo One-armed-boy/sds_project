8b833643f5940c1d5761b7fb86b57243546091bd
ChangMyeong <fanatic5500@gmail.com>
2022-05-24 15:39:55 +0900

    이메일, 비밀번호로 로그인 회원가입 구현

--- accounts/models.py
+++ accounts/models.py
@@ -5,22 +5,20 @@ from django.contrib.auth.models import BaseUserManager, AbstractUser, Permission
 
 class AppUserManager(BaseUserManager):
     # 유저 생성 시 create_user 함수가 작동
-    def create_user(self,email,password,nickname,**extra_fields):
+    def create_user(self,email,password,**extra_fields):
         # 사용하지 않는 나머지 값들은 **extra_fields로 모조리 받아 옴
         if not email:
-            raise ValueError(_('The Phone must be filled with'))
-        if '-' in email:
-            raise ValueError(_('The Phone must be filled with numeric, not "-"'))
-
-        user = self.model(email=email,nickname=nickname,**extra_fields)
+            raise ValueError(_('The email must be filled with'))
+        email=self.normalize_email(email) #이메일 정규화
+        user = self.model(email=email,**extra_fields)
 
         #password hash 및 유저 save
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self,email,password,nickname,**extra_fields):
-        user = self.model(email=email,nickname=nickname,**extra_fields)
+    def create_superuser(self,email,password,**extra_fields):
+        user = self.model(email=email,**extra_fields)
 
         user.is_active = True
         user.is_staff = True
@@ -40,14 +38,14 @@ class AppUser(AbstractUser, PermissionsMixin):
         verbose_name='고객'
         verbose_name_plural="고객들"
 
-    nickname=models.CharField(max_length=10,unique=True,verbose_name='별명')
+    #nickname=models.CharField(max_length=10,unique=True,verbose_name='별명')
     username = None
     # username 필드를 없애고, uique한 필드로 수정
-    email = models.CharField(max_length=20,unique=True,verbose_name='이메일')
+    email = models.CharField(max_length=50,unique=True)
 
     USERNAME_FIELD = 'email'
     # 각 유저를 구분짓는 USERNAME_FIELD의 값을 email 으로 지정 -> 자동적으로 Require
-    REQUIRED_FIELDS = ['nickname']
+    REQUIRED_FIELDS = []
     # 이전에는 AbstractBaseUser의 default값인 username을 통해 User를 구분했다.
     # 하지만 username=None으로 username필드를 없애고,
     # USERNAME_FIELD = 'email'를 통해 email이 해당 역할을 수행하도록 만들었다!!
@@ -58,4 +56,4 @@ class AppUser(AbstractUser, PermissionsMixin):
     is_staff =models.BooleanField(default=False)
     is_superuser =models.BooleanField(default=False)
     def __str__(self):
-        return self.nickname
+        return self.email
