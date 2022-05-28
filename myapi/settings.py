67ba9a00407def76636c5c2509986c5c624623c6
ChangMyeong <fanatic5500@gmail.com>
2022-05-28 14:40:43 +0900

    admin 관련 오류 해결

--- myapi/settings.py
+++ myapi/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 from django.core.exceptions import ImproperlyConfigured
-'''
+#'''
 secret_file = os.path.join(BASE_DIR, 'secrets.json')
 
 with open(secret_file) as f:
@@ -39,8 +39,8 @@ def get_secret(setting, secrets=secrets):
         raise ImproperlyConfigured(error_msg)
 
 SECRET_KEY = get_secret("SECRET_KEY")
-'''
-SECRET_KEY = os.environ.get('SECRET_KEY')
+#'''
+#SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG', 'True') != 'False')
 
@@ -168,13 +168,13 @@ LOGOUT_REDIRECT_URL='/'
 
 AUTH_USER_MODEL = 'accounts.AppUser'
 
-#ACCOUNT_USER_MODEL_USERNAME_FIELD = None
-#ACCOUNT_EMAIL_REQUIRED=True
-#ACCOUNT_UNIQUE_EMAIL=True
-#ACCOUNT_USERNAME_REQUIRED=False
-#ACCOUNT_AUTHENTICATION_METHOD='email'
-#SITE_ID=1
-#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+ACCOUNT_EMAIL_REQUIRED=True
+ACCOUNT_UNIQUE_EMAIL=True
+ACCOUNT_USERNAME_REQUIRED=False
+ACCOUNT_AUTHENTICATION_METHOD='email'
+SITE_ID=1
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # 아래는 이메일 인증관련 -> PHONEMUNECRYPT을 모조리 EMAIL로 고치면 이메일 인증 절차 가능
 #ACCOUNT_PHONEMUNECRYPT_VERIFICATION='mandatory'
 #ACCOUNT_CONFIRM_PHONEMUNECRYPT_ON_GET=True
