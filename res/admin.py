9b272d1526e4916bff34df9c3f896acfefe0bd71
ChangMyeong <fanatic5500@gmail.com>
2022-06-05 18:18:25 +0900

    admin 추가

--- res/admin.py
+++ res/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Res,Review
+from .models import Res,Review,reserve
 # Register your models here.
 
 admin.site.register(Res)
 admin.site.register(Review)
+admin.site.register(reserve)
